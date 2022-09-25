8afc6b3dc16b63e717119b73ed8d2afc316daeab
JuanRRodrigues <juancontato27@gmail.com>
2022-09-24 22:37:11 -0300

    Update JuanRibeiroRodrigues_LG2_ATIVIDADE_1.java

--- JuanRibeiroRodrigues_LG2_ATIVIDADE_1.java
+++ JuanRibeiroRodrigues_LG2_ATIVIDADE_1.java
@@ -7,7 +7,7 @@ Report the total amount of money spent
 
 Student: Juan Ribeiro Rodrigues / PRONT:SP309300X / 
 
-SUBTITLE:  Y->Year   D->DAY  C-> BAG COST  T->MONEY SPENT
+SUBTITLE:  Y->Year   D->DAY  C-> BAG COST  T->MONEY SPENT    R -> 21 cigarettes = 2xC
 */
 
 import java.util.Scanner; 
@@ -17,15 +17,24 @@ public class Main
 	public static void main(String[] args) {                             
 		System.out.println("How many years have you been a smoker?");
 		Scanner in = new Scanner(System.in);
-		int Y, D, C, T;
+		double Y, D, C, T, R;
 		Y = in.nextInt(); 
 		System.out.println("How many cigarettes do you smoke per day?");
 		D = in.nextInt();
 		System.out.println("How much does a cigarette bag cost?");
 	    C = in.nextInt();
 		T = C*((Y*(365*D)/20));
+		R = T%20;
+	
+		if (R>0){   
+		    T = T+C;
+		}
 		System.out.println("This person spent on cigarettes: "+T);
+		
+		
 	}
 }
+
+
 
 
