7b35dda4c9807de8f3a78024e8ae1394d6714fee
JuanRRodrigues <juancontato27@gmail.com>
2022-09-24 22:37:00 -0300

    Create Curso.java

---
+++ Escola/src/Curso.java
@@ -0,0 +1,18 @@
+
+public class Curso {
+
+	private String NomeCurso =  "Direito";
+	//private String NomeCurso2 =  "Geografia";
+	private String CargaHoraria =  "3.700 horas";
+	private String Duraçao = "5 Anos";
+	
+	String getCurso() {
+		return NomeCurso;
+	}
+	String getCargaHorária() {
+		return CargaHoraria;
+	}
+	String getDuração() {
+		return Duraçao; 
+	}
+}
