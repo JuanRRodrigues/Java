f78cda608d7dc75c1aeceef5b5a7fd1b0a5eea22
JuanRRodrigues <juancontato27@gmail.com>
2022-09-27 21:15:33 -0300

    Update Boletim.java

--- Escola/src/Boletim.java
+++ Escola/src/Boletim.java
@@ -1,7 +1,30 @@
 
 public class Boletim {
 	
-	private int nota;
-	private String Situaçao;
-
+	private int Nota1;
+	
+	
+	
+	public Boletim (int Nota1) {
+		setNota1(Nota1);
+		
+		
+	}
+	public void setNota1(int notaUm) {
+		this.Nota1 = notaUm;
+		
+		
+	}
+	public int getNota1() {
+		return Nota1;
+		
+	}
+	
+	
+	public String informarNotas() {
+		return "Nota 1: "+ getNota1();
+			  
+			 
+		
+	}
 }
