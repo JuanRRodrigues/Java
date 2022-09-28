e301aaa61e127aacced7cf48a102802532089f5a
JuanRRodrigues <juancontato27@gmail.com>
2022-09-27 21:15:38 -0300

    Update Disciplina.java

--- Escola/src/Disciplina.java
+++ Escola/src/Disciplina.java
@@ -1,7 +1,40 @@
 
 public class Disciplina {
 	
+	private String NomeDisciplina;
+	private int Semestre;
+	private int Nota;
 	
+	
+	public Disciplina () {
+		setNomeDisciplina(NomeDisciplina);
+		setSemestre( Semestre);
+		setNota(Nota);
+		
+	}
+	public void setNomeDisciplina(String Nomed) {
+		this.NomeDisciplina = Nomed;
+		
+		
+	}
+	public String getNomeDisciplina() {
+		return this.NomeDisciplina;
+		
+	}
+	public void setSemestre(int Semestred) {
+		this.Semestre = Semestred;
+			
+	}
+	public int getSemestre() {
+		return this.Semestre;
+		
+	}
+	public void setNota(int Notad) {
+		this.Nota = Notad;
 	}
 	
-
+	public int getNota() {
+		return this.Nota;
+	}
+   
+}
